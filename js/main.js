0874029e919b7ae5aa0159751268b14c32b40ef2
alibabayev2002 <ali.babayev.655.19@gmail.com>
2020-10-30 20:57:09 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 Object.prototype.sidebar = function (width,selecter) {
+    $('head').append('<style>.sidebar{left:0px !important;transform: translateX(0px) !important;opacity: 1 !important;}.sidebar-invisible{transition: 400ms all;opacity: 0;top: 0;position: fixed;height: 100vh !important;border-right: 1px solid black;}.sidebar-body{transition: 400ms all;padding-left: 300px;}</style>')
     $(selecter).addClass('sidebar-invisible');
     const invisibleSidebar = $('.sidebar-invisible')
     invisibleSidebar.css('width' , width + 'px')
